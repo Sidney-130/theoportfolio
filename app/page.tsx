e618f4ab404478d1e5baba278a7cd23b3626c77f
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-03 08:38:08 +0100

    page done

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomePage from "@/components/layout/home";
 
 export default function Home() {
   return (
-    <div className="mt-20 flex flex-col justify-between gap-15 px-4 md:px-20">
+    <div className="mt-20 flex flex-col justify-between gap-15 md:gap-30 px-4 md:px-20">
       <HomePage />
       <About />
     </div>

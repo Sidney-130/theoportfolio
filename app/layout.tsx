e446376ebf6cda743c32492ef73a92df9da59a60
Sidney-130 <akabuoguoluebube8@gmail.com>
2025-09-03 08:41:39 +0100

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
   title: "Theo's Portfolio",
-  description: "Technical Writer, Secial Media/Content Manager.",
+  description: "Technical Writer, Social Media/Content Manager.",
 };
 
 export default function RootLayout({
@@ -25,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.variable} antialiased`}>
         <Navbar />
         {children}
       </body>
